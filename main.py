daee66d327da5e34b66378c2630de55c6de332fe
Awaniliar <Awaniliar8@gmail.com>
2026-05-15 19:54:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import *
+from city2 import *
+
+ground()
+sky()
+moon()
+clouds1()
+clouds2()
+road()
+pedestrian_walk()
+office_buildings()
+lights1()
+lights2()
+lights3()
+
+exitonclick()
